5ee27bc4ca0ae44ee2598ec0eb17e60953f83af1
*твое <*>
2023-04-27 19:28:02 +0500

    Change hw-6 new

--- hw-6.js
+++ hw-6.js
@@ -53,13 +53,13 @@ console.log(realDate);
 function fructShow () {
 const fruct = ['Яблоко', 'Груша', 'Дыня', 'Виноград', 'Персик', 'Апельсин', 'Мандарин'];
 alert(fruct.sort(() => Math.random() - 0.5));
-let apple = prompt('Чему равнялся первый элемент массива?');
-let pineapple = prompt('Чему равнялся последний элемент массива?') ;
-if (apple == 'Яблоко' && pineapple == 'Мандарин'){
+let answerOne = prompt('Чему равнялся первый элемент массива?');
+let answerTwo = prompt('Чему равнялся последний элемент массива?') ;
+if (answerOne == fruct[0] && answerTwo == fruct[fruct.length - 1]){
     return alert('Поздравляю, Вы угадали!');
-} else if (apple == 'Яблоко' && pineapple == 'Апельсин' || apple ==  'Груша' && pineapple == 'Мандарин' ){
+} else if (answerOne === array[0] || answerTwo === array[array.length - 1]){
     return alert ('Вы были близки к победе!');
-} else if (apple == 'Дыня', 'Виноград', 'Персик' && pineapple == 'Персик', 'Виноград', 'Дыня' ){
+} else {
     return alert('Неправильно');
 }
 }
